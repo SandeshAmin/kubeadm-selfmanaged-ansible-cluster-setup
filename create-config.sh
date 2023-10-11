b49330966705a79f89c02627cf955060cc9edfda
Sandesh KV <sandeshkv92@gmail.com>
2023-10-11 11:54:08 +0530

    Update create-config.sh

--- create-config.sh
+++ create-config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@167.71.236.224 mkdir /home/ubuntu/.kube
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@167.71.236.224 sudo cp /etc/kubernetes/admin.conf /home/ubuntu/.kube/config
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@167.71.236.224 sudo chmod +r /home/ubuntu/.kube/config
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@167.71.236.224:/home/ubuntu/.kube/config ~/.kube
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@$CONTROLIP mkdir /home/ubuntu/.kube
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@$CONTROLIP sudo cp /etc/kubernetes/admin.conf /home/ubuntu/.kube/config
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@$CONTROLIP sudo chmod +r /home/ubuntu/.kube/config
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@$CONTROLIP:/home/ubuntu/.kube/config ~/.kube
