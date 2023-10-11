c3f8ed68693a56467b9c813669a90b2326c33464
Sandesh KV <sandeshkv92@gmail.com>
2023-10-11 11:55:22 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ssh-keygen -R 167.71.236.224
-ssh-keygen -R 64.227.150.190
-ssh-keygen -R 167.71.230.69
+ssh-keygen -R $CONTROLIP
+ssh-keygen -R $DATAPLANE_IP1
+ssh-keygen -R $DATAPLANE_IP2
 
 
 ssh-keygen -R control1
